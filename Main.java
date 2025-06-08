a39732c685ef3f6f587ba76281cdb8bdf05fe2ad
Kathy Dong <kathy@Kathys-MacBook-Air.local>
2025-06-08 13:51:17 -0600

    yippie

---
+++ Main.java
@@ -0,0 +1,15 @@
+/*
+public class Main {
+    public static void main(String[] args) {
+        Cube cubeTrial = new Cube(4);
+        cubeTrial.findVolume();
+    }
+}
+*/
+
+public class Main {
+        public static void main(String[] args) {
+            Cube cubeTrial = new Cube(3);
+            cubeTrial.findVolume();
+        }
+}
